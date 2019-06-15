78d5a44938ee0796fb4b96af733b21bc9125edb2
Evan Tomczyk <tomczykej@gmail.com>
2019-06-15 20:40:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHitoGrandma(string) {
+  if string === lowercase {
+    return "I can't hear you!";
+  }
+}
